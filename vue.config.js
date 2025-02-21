fd42441d943a7cdad8db618a3df8e471d73eefec
teddy <ykcode@163.com>
2025-02-21 17:16:24 +0800

    修改编译

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require('@vue/cli-service')
+const TerserPlugin = require('terser-webpack-plugin')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: false, // 如果需要移除 console.log 可以设置为 true
+            },
+            output: {
+              comments: false, // 移除所有注释
+            },
+          },
+        }),
+      ],
+    },
+  },
 })
